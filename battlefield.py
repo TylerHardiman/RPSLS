4a83f2ca265a5d60a2777e5df75777e88b7f945d
colemassey <cole.massey33@gmail.com>
2022-01-10 11:07:29 -0600

    gameplay while loop, player turn, player options

--- battlefield.py
+++ battlefield.py
@@ -1,15 +1,48 @@
-class battlefield():
-    def __init__(): # self.player_one = Human()
-                    # self.player_two = Human() or AI() we would need to setup a input and if elif to setup how many players based off of selection
+from human import Human
+from ai import Ai
+
+class Battlefield():
+    def __init__(self):
+        self.player_one = Human()
+        self.player_two = Human() or Ai()
+        # We would need to setup a input and if elif to setup how many players based off of selection
     
-    def run_game()
+    def run_game():
+        pass
+
 
     def game_welcome_and_rules(self):
         print('Welcome to Rock, Paper, Scissors, Lizard, Spock!')
         print('')
     
     def game_play(): # remember first player with 2 round wins is the winner of best of 3
-                    # check first to see if its a tie, then check all of the win conditions for player 1 finallay our else can be player 2 wins 
-        while len(self.fleet.robots) > 0 and len(self.herd.dinosaur) > 0:
+                    # check first to see if its a tie, then check all of the win conditions for player 1 finally our else can be player 2 wins 
+        while self.player_one_wins < 2 and self.player_two_wins < 2:
+            self.player_one_turn()
+            if len(self.player_two_wins) == 0:
+                pass
+            else:
+                self.player_two_turn
+
+    def player_turn():
+        # input
+        # method that adds wins. ex win = + 1 tie = + 0
+        # show option
+
+    def player_options():
+        # inheritance to player turn(?)
+        print('Current Dino Herd')
+        index = 0
+        for dino in self.herd.dinos:
+            print(f'Press {index} to select {dino.name} ({dino.health} health)')
+            index += 1
+
 
     def winner():
+            if self.player_two_wins == 0:
+                print(f"")
+                print(f"Player One WINS!")
+            else:
+                print("")
+                print(f"Player WINS!")
+                pass
--- main.py
+++ main.py
@@ -1,2 +1,8 @@
+from battlefield import Battlefield
+battlefield = Battlefield()
+
+battlefield.run_game()
+
+
 
     
